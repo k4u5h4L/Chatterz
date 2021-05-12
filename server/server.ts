995e3300b95dafa6a2136cd1a8badf3c7dae5679
k4u5h4L <kaushal.v.bhat@gmail.com>
2021-05-12 20:56:20 +0530

    Add server with web sockets

---
+++ server/server.ts
@@ -0,0 +1,33 @@
+import { createServer } from "http";
+import cors from "cors";
+import express, { Request, Response } from "express";
+import { Server, Socket } from "socket.io";
+
+const app = express();
+
+const httpServer = createServer();
+
+const options = {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+};
+const io = new Server(httpServer, options);
+
+app.use(cors());
+
+app.get("/api", (req: Request, res: Response) => {
+    res.send({ message: "you've reached api root!" });
+});
+
+io.on("connection", (socket: Socket) => {
+    console.log("connected");
+    socket.on("chat", (arg) => {
+        console.log(arg);
+        socket.emit("chat", "I see you have connected");
+    });
+});
+
+httpServer.listen(8000, () => console.log(`server is running on port 8000`));
+// WARNING !!! app.listen(8000); will not work here, as it creates a new HTTP server
